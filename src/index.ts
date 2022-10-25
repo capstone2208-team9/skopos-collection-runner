4af7b607450f767d7a3fd1f3420250ceaee23a8c
Hans Elde <hanserik.elde@gmail.com>
2022-10-24 19:35:29 -0700

    added cors
    Co-authored-by: Gagan Sapkota <here.gagan@gmail.com>
    Co-authored-by: Nykaela Dodson <nykaelad@gmail.com>
    Co-authored-by: Katherine Ebel <kathyebel@me.com>

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,25 @@
 import { interpret } from 'xstate';
 import { waitFor } from 'xstate/lib/waitFor.js';
+import cors from 'cors'
 
 import { collectionRunnerMachine } from './entities/collectionRunnerMachine.js';
 import express from 'express'
 const app = express()
+app.use(cors())
 const PORT = 3003
 
 app.get('/:id', async (req, res) => {
   const collectionId = Number(req.params.id)
+
   const collectionRunnerService = interpret(collectionRunnerMachine)
     .onTransition(state => console.log(state.value, state.context)) // FOR LOGGING
+
   collectionRunnerService.start()
   collectionRunnerService.send({ type: 'QUERY', collectionId: collectionId })
   await waitFor(collectionRunnerService, (state) => state.matches('complete'))
   collectionRunnerService.stop()
+
+  res.header('Access-Control-Allow-Origin', '*');
   res.sendStatus(200)
 })
 
