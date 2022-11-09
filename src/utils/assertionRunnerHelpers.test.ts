2f4e1254c4cb6ca668e0901d6e7ccf6e024cb4c3
Gagan Sapkota <here.gagan@gmail.com>
2022-11-09 11:18:36 -0800

    assertion parsing works correctly
    1. added types
    2. added tests for checking the logic
    3. cleaned up the functions
    
    Co-Authored-By: Katherine Ebel <kathyebel@me.com>
    Co-Authored-By: Nykaela Dodson <nykaelad@gmail.com>
    Co-Authored-By: Hans Elde <hanserik.elde@gmail.com>

--- src/utils/assertionRunnerHelpers.test.ts
+++ src/utils/assertionRunnerHelpers.test.ts
@@ -1,20 +1,175 @@
-import {isPassing} from './assertionRunnerHelpers'
+import {
+  isAssertionPassing,
+  interpolateReferences,
+  Response,
+  Assertion,
+  AssertionResult,
+  invokeCheckAssertions,
+} from "./assertionRunnerHelpers";
 
-describe('parseBody', () => {
-  it('should pass smoke test', function () {
-    expect(true).toBe(true)
-  })
-})
+function mockValues() {
+  const assertions: Assertion[] = [
+    {
+      property: "body.country[1].name",
+      comparison: "is equal to",
+      expected: "United States of America",
+      id: 4,
+    },
+    {
+      property: "body.country[1].name",
+      comparison: "is equal to",
+      expected: "Australia",
+      id: 4,
+    },
+    {
+      property: "headers.Content-Type",
+      comparison: "is equal to",
+      expected: "application/json",
+      id: 4,
+    },
+    {
+      property: "latency",
+      comparison: "is less than",
+      expected: 458,
+      id: 4,
+    },
+  ];
 
-describe('isAssertionPassing', () => {
-  it('should return true for 200 is equal to 200', function () {
-    const expected = '200'
-    const actual = expected
-    const operator = 'is equal to'
-    expect(isPassing(actual, operator, expected)).toBe(true)
-  })
-})
+  const response: Response = {
+    id: 1,
+    status: 200,
+    body: {
+      country: [
+        { country_id: "AU", name: "Australia" },
+        { country_id: "US", name: "United States of America" },
+      ],
+    },
+    headers: {
+      "Content-Type": "application/json",
+    },
+    latency: 352,
+    request: {
+      assertions,
+    },
+  };
 
-describe('assertionFailed', () => {
-  
-})
+  const interpolatedAssertions: Assertion[] = [
+    {
+      property: "United States of America",
+      comparison: "is equal to",
+      expected: "United States of America",
+      id: 4,
+    },
+    {
+      property: "United States of America",
+      comparison: "is equal to",
+      expected: "Australia",
+      id: 4,
+    },
+    {
+      property: "application/json",
+      comparison: "is equal to",
+      expected: "application/json",
+      id: 4,
+    },
+    {
+      property: 352,
+      comparison: "is less than",
+      expected: 458,
+      id: 4,
+    },
+  ];
+
+  const assertionResults = [
+    {
+      responseId: 1,
+      pass: true,
+      actual: "United States of America",
+      assertionId: 4,
+    },
+    {
+      responseId: 1,
+      pass: false,
+      actual: "United States of America",
+      assertionId: 4,
+    },
+    {
+      responseId: 1,
+      pass: true,
+      actual: "application/json",
+      assertionId: 4,
+    },
+    {
+      responseId: 1,
+      pass: true,
+      actual: 352,
+      assertionId: 4,
+    },
+  ];
+
+  return { assertions, response, interpolatedAssertions, assertionResults };
+}
+
+describe("isAssertionPassing function returns correct boolean", () => {
+  it("should pass smoke test", function () {
+    expect(true).toBe(true);
+  });
+
+  it("should return true for 200 is equal to 200", function () {
+    const expected = "200";
+    const actual = expected;
+    const operator = "is equal to";
+    expect(isAssertionPassing(actual, operator, expected)).toBe(true);
+  });
+
+  it("should return false for 200 is equal to 202", function () {
+    const expected = "200";
+    const actual = 202;
+    const operator = "is equal to";
+    expect(isAssertionPassing(actual, operator, expected)).toBe(false);
+  });
+
+  it("should return true for 300 is less than 400", function () {
+    const expected = "400";
+    const actual = 300;
+    const operator = "is less than";
+    expect(isAssertionPassing(actual, operator, expected)).toBe(true);
+  });
+
+  it("should return false for 600 is less than 400", function () {
+    const expected = "400";
+    const actual = 600;
+    const operator = "is less than";
+    expect(isAssertionPassing(actual, operator, expected)).toBe(false);
+  });
+
+  it("should return false for 600 is greater than 400", function () {
+    const expected = "400";
+    const actual = 600;
+    const operator = "is greater than";
+    expect(isAssertionPassing(actual, operator, expected)).toBe(true);
+  });
+
+  it("should return true for undefined is equal to undefined", function () {
+    const expected = "undefined";
+    const actual = undefined;
+    const operator = "is equal to";
+    expect(isAssertionPassing(actual, operator, expected)).toBe(true);
+  });
+});
+
+describe("interpolate values from responses", () => {
+  const { assertions, response, interpolatedAssertions } = mockValues();
+  it("should interpolate response", function () {
+    expect(interpolateReferences(assertions, response)).toEqual(
+      interpolatedAssertions
+    );
+  });
+});
+
+describe("assertion results are correct for a response", () => {
+  const { response, assertionResults } = mockValues();
+  it("should interpolate response", function () {
+    expect(invokeCheckAssertions(response)).toEqual(assertionResults);
+  });
+});
--- src/utils/assertionRunnerHelpers.ts
+++ src/utils/assertionRunnerHelpers.ts
@@ -1,12 +1,41 @@
-import { gqlMutateCreateAssertionResults } from '../services/queries'
+import { gqlMutateCreateAssertionResults } from "../services/queries";
+
+export type BasicValue = string | number | undefined;
+export type Assertion = {
+  property: BasicValue;
+  comparison: string;
+  expected: BasicValue;
+  id: number;
+};
+
+export type Response = {
+  request: {
+    assertions: Assertion[];
+  };
+  status: number;
+  latency: number;
+  body: any;
+  headers: Record<string, any>;
+  id: number;
+};
+
+export type AssertionResult = {
+  responseId: number;
+  pass: boolean;
+  actual: BasicValue;
+  assertionId: number;
+};
 
 export function isPassing(
-  actual: any,
+  actual: BasicValue,
   operator: string,
-  expected: any
+  expected: BasicValue
 ): boolean {
-  if (typeof actual === 'number') {
-    expected = Number(expected)
+  if (typeof actual === "number") {
+    expected = Number(expected);
+  } else {
+    //if not number, the undefined value should be converted to string for comparison
+    actual = String(actual);
   }
 
   switch (operator) {
@@ -16,18 +45,20 @@ export function isPassing(
       return actual > expected;
     case "is less than":
       return actual < expected;
-    case "includes":
-      return actual.includes(expected);
+    default:
+      return false;
   }
 }
 
-function isAssertionPassing(
-  leftOperand: any,
+export function isAssertionPassing(
+  leftOperand: BasicValue,
   operator: string,
-  rightoperand: any
+  rightoperand: BasicValue
 ): boolean {
   if (operator.match(/null/)) {
-    rightoperand = null;
+    //bc for something to be null means that the key is not present in the object
+    //hence, getting the value for that key returns undefined
+    rightoperand = undefined;
     operator.replace("null", "equal to");
   }
 
@@ -40,74 +71,79 @@ function isAssertionPassing(
   return isPassing(leftOperand, operator, rightoperand);
 }
 
-const parseBody = (identifier: string, response: any): any => {
+const parseResponse = (identifier: string, response: Response): BasicValue => {
   let path = identifier.split(/\.|\[|\]/).filter((item) => item !== "");
-  let currentElement = response;
+  let currentElement: any = response;
 
+  console.log("path", path);
   for (let step of path) {
     if (!currentElement[step]) {
-      return undefined
+      return undefined;
     }
+    console.log("current element", currentElement);
     currentElement = currentElement[step];
   }
 
   return currentElement;
 };
 
+export const interpolateReferences = (
+  listOfAssertions: Assertion[],
+  response: Response
+): Assertion[] => {
+  const interpolateResponseReference = (assertion: Assertion) => {
+    let property = assertion.property;
+    let identifier: string = String(property);
+    let optionToParse: BasicValue[] = ["body", "headers"];
 
-export const interpolateReferences = (listOfAssertions: any[], response): any[] => {
-  const interpolateBodyReference = (assertion) => {
-    let property = assertion.property
-
-    if (property.includes('body')) {
-      property = parseBody(property, response)
+    if (identifier.includes("body") || identifier.includes("headers")) {
+      property = parseResponse(identifier, response);
+    } else {
+      property = response[property];
     }
 
     return {
       ...assertion,
-      property
-    }
-  }
-
-  return listOfAssertions.map(interpolateBodyReference)
-}
-
-export const invokeCheckAssertions = async (
-  response
-): Promise<any[]> => {
-  let assertionResults = [];
-
-  const assertionVerdict = (response) => {
-    const listOfAssertions = response.request.assertions;
-    listOfAssertions.forEach((assertion) => {
-      const {
-        property,
-        comparison,
-        expected,
-        id,
-      } = assertion;
-      let actual = parseBody(property, response);
-      let pass = isAssertionPassing(actual, comparison, expected);
-      assertionResults.push({
-        responseId: response.id,
-        pass,
-        actual,
-        assertionId: id
-      });
-    });
+      property,
+    };
   };
 
-  assertionVerdict(response)
-  return assertionResults;
+  return listOfAssertions.map(interpolateResponseReference);
 };
 
-export const invokeSaveAssertionResults = async (listOfAssertionResults) => {
-  return await gqlMutateCreateAssertionResults(listOfAssertionResults)
-}
+export const invokeCheckAssertions = (
+  response: Response
+): AssertionResult[] => {
+  const listOfAssertions = interpolateReferences(
+    response.request.assertions,
+    response
+  );
+
+  const assertionToAssertionResult = (assertion: Assertion) => {
+    const { property, comparison, expected, id } = assertion;
+    let actual = property;
+    let pass = isAssertionPassing(actual, comparison, expected);
+
+    return {
+      responseId: response.id,
+      pass,
+      actual,
+      assertionId: id,
+    };
+  };
+
+  return listOfAssertions.map(assertionToAssertionResult);
+};
+
+export const invokeSaveAssertionResults = async (
+  listOfAssertionResults: AssertionResult[]
+) => {
+  return await gqlMutateCreateAssertionResults(listOfAssertionResults);
+};
 
 export const assertionFailed = (context, event) => {
   for (let i = 0, len = context.assertionResults.length; i < len; i += 1) {
-    if (context.assertionResults[i]['pass'] === false) return true;
+    if (context.assertionResults[i]["pass"] === false) return true;
   }
   return false;
-}
+};
